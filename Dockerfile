538f69d0f0105a9d92f3417b8779c2143db552dd
Namitos <namit@yandex.ru>
2018-08-20 12:13:06 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:latest
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+COPY . /usr/src/app
+
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - &&\
+  sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' &&\
+  apt-get update &&\
+  apt-get install -y git make google-chrome-unstable
+
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+RUN npm install --unsafe-perm
+
+CMD [ "node", "app" ]
---
+++ app.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const port = process.env.port || 7000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const puppeteer = require('puppeteer');
+
+let app = express();
+let server = http.createServer(app);
+app.use(bodyParser.json());
+
+
+async function makeScreenshot(data) {
+  data.width = data.width || 800;
+  data.height = data.height || 600;
+  let { width, height } = data;
+  let browser = await puppeteer.launch({
+    headless: true,
+    //executablePath: '/opt/google/chrome/chrome',
+    executablePath: 'google-chrome-unstable',
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-infobars', `--window-size=${ width },${ height }`]
+  });
+  let page = await browser.newPage();
+  if (data.url) {
+    await page.goto(data.url, { waitUntil: 'networkidle2' });
+  } else if (data.html) {
+    await page.setContent(data.html);
+  }
+  let buffer = await page.screenshot();
+  await browser.close();
+  return { result: buffer.toString('base64') };
+}
+
+app.all('/', async (req, res) => {
+  try {
+    let q = req.method === 'GET' ? JSON.parse(req.query.q) : req.body;
+    let r = await makeScreenshot(q);
+    res.send(r);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
+});
+
+server.listen(port, () => {
+  console.log(`chrome-screenshot started on ${port}`);
+});
